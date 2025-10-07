7cc9f0ffbfff76fe9d26a427ee960e3113c98564
anujyadav070 <annujyaadavv@gmail.com>
2025-10-07 21:06:22 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -7,8 +7,25 @@ dotenv.config({
 })
 
 
-
+//when database is connected it return a promise so use .then & .catch
 connectDB()
+    .then(() => {
+       
+
+        const server = app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port ${process.env.PORT}`);
+        })
+
+        server.on("error", (error) => {
+            console.error("ERRR:", error)
+            throw error
+        })
+
+    })
+    .catch(() => {
+        console.log(`mongo db connection failed!!`, error)
+
+    })
 
 /*
 import express from "express"
